f9dde809c0a9a40bf14680f33b76b50727b1f9b0
AmadoCleto <94912927+ArtemioVibora@users.noreply.github.com>
2025-05-17 20:31:47 +0800

    Add files via upload

---
+++ OOPCSharp/Driver.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Driver
+{
+    public static void Main(string[] a)
+    {
+        Person person1 = new Person("Amado");
+        Console.WriteLine(person1.getName());        
+
+    }
+}
---
+++ OOPCSharp/Person.cs
@@ -0,0 +1,15 @@
+public class Person
+{
+    private string name;
+
+    public Person(string name)
+    {
+        this.name = name;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+}
